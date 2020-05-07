1ea61e6e4ea8bcc37d75e8ba64139ccc15851c7e
Nikoda <nikodamiura@gmail.com>
2020-05-07 03:17:20 +0300

    upgrade and optimaze code :)

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 const aParalase = document.querySelector('.aParalase'),
     pImage = aParalase.querySelector('.pImage');
     aParalase.onmousemove = (e) => {
-        pImage.style.transform = `rotateX(${-(e.offsetY - (pImage.offsetWidth / 2)) / 10}deg) rotateY(${(e.offsetX - (pImage.offsetWidth / 2)) / 10}deg)`;
+        pImage.style.transform = `rotateX(${-(e.offsetY - (pImage.offsetWidth / 2)) / 12}deg) rotateY(${(e.offsetX - (pImage.offsetWidth / 2)) / 12}deg)`;
     }
     aParalase.onmouseout = (e) => {
         pImage.style.transform = 'rotate(0deg)';
